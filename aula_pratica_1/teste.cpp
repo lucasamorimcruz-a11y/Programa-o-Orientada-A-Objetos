9c707a28f62093d9916cfb08f4c2d0c68b125b08
lucasamorimcruz-a11y <lucas.amorim.cruz@gmail.com>
2026-03-12 18:22:41 -0300

    Update teste.cpp

--- aula_pratica_1/teste.cpp
+++ aula_pratica_1/teste.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct person{
+typedef struct person{
     int age;
     string name;
 
 } person_t;
 int main(){
-    vector<person> array (5);
+    vector<person_t> array (5);
     for (auto &x : array) {
         string s; cout << "Idade: "; getline(cin, s); x.age = stoi(s);
         cout << "Nome: "; getline(cin, x.name);
     }
-    sort(array.begin(), array.end(), [] (const person &a, const person &b){
+    sort(array.begin(), array.end(), [] (const person_t &a, const person_t &b){
         return a.age > b.age;
     });
     for (auto &x : array){
