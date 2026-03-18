deda05916b44fa0a7d62e33b5f8655deb1c6cc87
Lucas Amorim <lucas.amorim.cruz@gmail.com>
2026-03-18 16:49:41 -0300

    Update teste.cpp

--- aula_pratica_1/teste.cpp
+++ aula_pratica_1/teste.cpp
@@ -8,7 +8,7 @@ typedef struct person{
 int main(){
     vector<person_t> array (5);
     for (auto &x : array) {
-        int age;out << "Idade: "; cin >> age; x.age = age; cout << '\n'; cout << "Nome: "; getline(cin, x.name); 
+        int age; cout << "Idade: "; cin >> age; x.age = age; cout << '\n'; cout << "Nome: "; getline(cin, x.name); 
     }
     sort(array.begin(), array.end(), [] (const person_t &a, const person_t &b){
         return a.age > b.age;
