6d33267777459030be665a1aa295743838492437
Lucas Amorim <lucas.amorim.cruz@gmail.com>
2026-03-18 16:49:24 -0300

    Update teste.cpp

--- aula_pratica_1/teste.cpp
+++ aula_pratica_1/teste.cpp
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 typedef struct person{
     int age;
     string name;
-
 } person_t;
+
 int main(){
     vector<person_t> array (5);
     for (auto &x : array) {
-        string s; cout << "Idade: "; getline(cin, s); x.age = stoi(s);
-        cout << "Nome: "; getline(cin, x.name);
+        int age;out << "Idade: "; cin >> age; x.age = age; cout << '\n'; cout << "Nome: "; getline(cin, x.name); 
     }
     sort(array.begin(), array.end(), [] (const person_t &a, const person_t &b){
         return a.age > b.age;
